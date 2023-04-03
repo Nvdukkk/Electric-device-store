496c3184e10d0735d89c739d85cad62a67a00714
Nvdukkk <103177727+Nvdukkk@users.noreply.github.com>
2023-04-03 17:26:17 +0700

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Route , Switch } from 'react-router-dom'; 
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import Details from './components/Details';
-import Cart from './components/Cart'; 
-import Default from './components/Default';
-import Modal from './components/Modal';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Switch>
-        <Route exact path="/" component={ProductList} />
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/product" component={ProductList} />
         <Route path="/details" component={Details} />
         <Route path="/cart" component={Cart} />
         <Route component={Default} />
       </Switch>
-      <Modal />  
+      <Modal />
     </React.Fragment>
-  ); 
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,12 @@ export default class Navbar extends Component {
     return (
       <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
-          <img src={logo} alt="store" className="navbar-brand" />
+          <img src={logo} alt="logo" className="navbar-brand" />
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">
-              products
+            <Link to="/product" className="nav-link">
+              Cửa hàng
              </Link>
           </li>
         </ul>
